ed623a5e89304a9ff80aebdb8d77bc073daea191
zy <xpzouying@gmail.com>
2021-09-16 04:47:11 +0000

    更新备注

--- stock.go
+++ stock.go
@@ -10,7 +10,7 @@ const (
 )
 
 type Stock struct {
-	Symbol string    `json:"symbol"` // 股票代码
+	Symbol string    `json:"symbol"` // 股票代码（SZ000858，SH600519)
 	Name   string    `json:"name"`   // 股票中文名字
 	Type   StockType `json:"type"`   // 股票上市地方
 }
